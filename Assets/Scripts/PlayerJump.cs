4c7e8d0df90ba2f5b1b7cdf322dd90fc5a31a89c
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-06-28 08:52:06 -0400

    animations, art

--- Assets/Scripts/PlayerJump.cs
+++ Assets/Scripts/PlayerJump.cs
@@ -16,10 +16,15 @@ namespace DefaultNamespace
         private float _jumpTime;
         private bool _isJumping;
         private float _lastJumpTime;
-        
+        private Animator _animator;
+        private static readonly int Jump = Animator.StringToHash("Jump");
+        private static readonly int FallY = Animator.StringToHash("fallY");
+        private static readonly int Land = Animator.StringToHash("Land");
+
         private void Start()
         {
             _rigidbody2D = GetComponent<Rigidbody2D>();
+            _animator = GetComponent<Animator>();
         }
 
         private void Update()
@@ -34,11 +39,15 @@ namespace DefaultNamespace
                 // Reset Y velocity on landing
                 if(_rigidbody2D.velocity.y < 0)
                 {
+                    _animator.SetTrigger(Land);
                     _rigidbody2D.velocity = new Vector2(_rigidbody2D.velocity.x, 0);
+                    _animator.SetFloat(FallY, 0);
                 }
                 if (Input.GetAxisRaw("Vertical") > 0 && Time.time - _lastJumpTime >= jumpCooldown)
                 {
                     _isJumping = true;
+                    _animator.ResetTrigger(Land);
+                    _animator.SetTrigger(Jump);
                     _jumpTime = 0;
                     _rigidbody2D.AddForce(Vector2.up * initialJumpForce, ForceMode2D.Impulse);
                     _lastJumpTime = Time.time;
@@ -54,6 +63,12 @@ namespace DefaultNamespace
             {
                 // The player is no longer jumping when they let go of the button
                 _isJumping = false;
+            }
+
+            if (hit.collider == null && _rigidbody2D.velocity.y < 0)
+            {
+                _animator.ResetTrigger(Jump);
+                _animator.SetFloat(FallY, _rigidbody2D.velocity.y);
             }
         }
     }
