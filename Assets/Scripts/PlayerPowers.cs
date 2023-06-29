371bd31a015779eaee9a5d84f289131a9588703f
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-06-29 10:47:47 -0400

    ignite, dash, better jumping (I think)

---
+++ Assets/Scripts/PlayerPowers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class PlayerPowers : MonoBehaviour
+    {
+        private Animator _playerAnimator;
+        private static readonly int Ignite = Animator.StringToHash("Ignite");
+
+        private void Start()
+        {
+            _playerAnimator = GetComponent<Animator>();
+        }
+
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Alpha1))
+            {
+                _playerAnimator.SetTrigger(Ignite);
+            }
+        }
+    }
+}
