5d9fc571aa02e2adc74492dd1e200a1c20518053
Mikail Miller <65094447+ironlionm4n@users.noreply.github.com>
2023-07-20 12:09:20 -0400

    slime animations, state machine

--- Assets/Scripts/EnemyScripts/EnemyMover.cs
+++ Assets/Scripts/EnemyScripts/EnemyMover.cs
@@ -1,37 +1,110 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using EnemyScripts;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] private float moveSpeed;
+    [SerializeField] private float idleTime = 3f;
+    [SerializeField] private Animator animator;
+
+    private EnemyStates _currentState;
     private Rigidbody2D _rigidbody2D;
+    private bool _isMoving;
+    private float _currentIdleTime;
+    private static readonly int IsMoving = Animator.StringToHash("IsMoving");
 
-    [SerializeField] private float moveTime;
+    #region UnityFunctions
 
-    private float _currentMoveTime;
-    // Start is called before the first frame update
-    void Start()
+    private void OnEnable()
     {
-        _currentMoveTime = moveTime;
+        EnemyHealth.EnemyHit += OnEnemyHit;
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _currentIdleTime = idleTime;
+        _currentState = EnemyStates.Idle;
         _rigidbody2D = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        _currentMoveTime -= Time.deltaTime;
-        if(_currentMoveTime <= 0)
+        switch (_currentState)
         {
-            _rigidbody2D.velocity = ChooseLeftOrRight() * (moveSpeed * Time.deltaTime);
+            case EnemyStates.Idle:
+                Idle();
+                break;
+            case EnemyStates.Moving:
+                if (!_isMoving) StartCoroutine(Moving());
+                break;
+            case EnemyStates.Hit:
+                HandleHit();
+                break;
         }
+    }
+
+    private void OnDisable()
+    {
+        EnemyHealth.EnemyHit -= OnEnemyHit;
+    }
+
+    #endregion
+
+    #region CustomFunctions
+
+    private void HandleHit()
+    {
+        StopCoroutine(Moving());
+        animator.SetBool(IsMoving, false);
+        UpdateCurrentState(EnemyStates.Idle);
+    }
+
+    private IEnumerator Moving()
+    {
+        _isMoving = true;
+        animator.SetBool(IsMoving, true);
+        var desiredDirection = ChooseLeftOrRight();
+        _rigidbody2D.velocity = desiredDirection * (moveSpeed * Time.deltaTime);
+        var timeToMove = Random.Range(2.5f, 4f);
+        yield return new WaitForSeconds(timeToMove);
+        _isMoving = false;
+        animator.SetBool(IsMoving, false);
+        UpdateCurrentState(EnemyStates.Idle);
+    }
+
+    private void Idle()
+    {
+        _currentIdleTime -= Time.deltaTime;
+        StopCoroutine(Moving());
+        _rigidbody2D.velocity = Vector2.zero;
+        if (_currentIdleTime <= 0)
+        {
+            _currentIdleTime = idleTime;
+            UpdateCurrentState(EnemyStates.Moving);
+        }
+    }
+
+    private void OnEnemyHit()
+    {
+        UpdateCurrentState(EnemyStates.Hit);
+    }
+
+    private void UpdateCurrentState(EnemyStates newState)
+    {
+        _currentState = newState;
     }
 
     private Vector2 ChooseLeftOrRight()
     {
         var flip = Random.Range(0, 2);
-        _currentMoveTime = moveTime;
         return flip == 0 ? Vector2.left : Vector2.right;
-        
     }
+
+    #endregion
 }
